a931543e7763b7b7923241fc36ff28cdfb04f0d3
YefersonA <yeferson.aguiar@correounivalle.edu.co>
2024-09-14 14:23:14 -0500

    acomodando el componente y la app

--- src/RecuperarContraseña.jsx
+++ src/RecuperarContraseña.jsx
@@ -1,18 +1,19 @@
 import react, {useState} from react
+import './RecuperarContraseña.css';
 
 const RecuperarContra =() =>{
     const [email,setEmail] = useState('');
     const [mensaje,setMensaje] = useState('');
 
     const recuperador = (e)=>{
-    e.defecto();
+    e.preventDefault();
     //to do logica recuperar contraseña
       setMensaje('se envio el enlace para recuperar la contraseña. ${email}');
     };
     return(
-    <div classname="Recuperar Contraseña">
+    <div classname="RecuperarContra">
      <h2>Recuperar Contraseña</h2>
-      <form onSubmit={defecto}>
+      <form onSubmit={recuperador}>
        <label htmlFor="email">Correo Electronico</label>
         <input 
         type="email"
@@ -24,9 +25,8 @@ const RecuperarContra =() =>{
             <button type ="submit">Enviar</button>
         </input>
       </form>
-    </div>
-    
-    );
-
-};
+    {mensaje && <p> {mensaje}
+      </p>}
+  </div>  
+);};
 export default RecuperarContra
